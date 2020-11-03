3503443b6804822f41aefb6e77e17a1447d8d740
kzcortes07 <kzcortes27@gmail.com>
2020-11-03 21:48:36 +0800

    assignment two

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "assign_two";
- ?>
+            class User
+            {
+              public $name;
+
+              function addFriend($name){
+                echo "$name" . " added a friend. <br>";
+              }
+              function postStatus($name){
+                echo "$name" . " posted a status. <br>";
+              }
+            }
+            $usernew = new User();
+            echo $usernew->addFriend('Ken Zedric Cortes');
+            echo "<br>";
+            echo $usernew->postStatus('Ken Zedric Cortes');
+
+      ?>
